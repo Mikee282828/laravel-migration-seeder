568f3a2256f69d8736aeb97dde5f7f03db9c95ee
michele laptop <micheledong2803@gmail.com>
2024-07-03 17:24:17 +0200

    seeder - manual

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
+use App\Models\Train;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -13,6 +14,16 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
-        //
+        $newTrain = new Train();
+        $newTrain->azienda = "provaSeeder";
+        $newTrain->stazione_di_partenza = "provastazionepartenza";
+        $newTrain->stazione_di_arrivo = "provastazionearrivo";
+        $newTrain->orario_di_partenza = "11:40";
+        $newTrain->orario_di_arrivo = "12:00";
+        $newTrain->codice_treno = "12394";
+        $newTrain->numero_carrozze = 5;
+        $newTrain->in_orario = 1;
+        $newTrain->cancellato=1;
+        $newTrain->save();
     }
 }
